2d5c20fd5362633ce21cec5e720588f3a1c6b549
baio <max.putilov@gmail.com>
2018-08-24 11:00:40 +0300

    date-range #73

--- libs/clr-controls/src/index.ts
+++ libs/clr-controls/src/index.ts
@@ -2,4 +2,6 @@ export * from './lib/date-time';
 export * from './lib/file-upload';
 export * from './lib/file-uploader';
 export * from './lib/image-upload';
+export * from './lib/date-time';
+export * from './lib/date-range';
 export * from './lib/select';
---
+++ libs/clr-controls/src/lib/date-range/index.ts
@@ -0,0 +1,2 @@
+export * from './date-range.component';
+export * from './date-range.module';
