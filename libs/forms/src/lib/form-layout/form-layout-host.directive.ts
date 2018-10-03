50709c155aa9611aef3b9ed4924b78ad5f35535e
baio <max.putilov@gmail.com>
2018-10-03 23:33:49 +0300

    form-layout setComponentProperties

--- libs/forms/src/lib/form-layout/form-layout-host.directive.ts
+++ libs/forms/src/lib/form-layout/form-layout-host.directive.ts
@@ -8,91 +8,15 @@ import {
     OnDestroy,
     OnInit,
     Type,
-    ViewContainerRef
+    ViewContainerRef,
+    ComponentRef
 } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import * as R from 'ramda';
 import 'reflect-metadata';
 import { Subject } from 'rxjs';
 import { IFormGroup } from '../models';
-
-/*
-
-const PROP_METADATA = '__prop__metadata__';
-const NG_METADATA_NAME = 'ngMetadataName';
-const NG_METADATA_NAME_INPUT = 'Input';
-const NG_METADATA_NAME_OUTPUT = 'Output';
-
-const getMeta = (comp: any) => {
-    const constructor = comp['constructor'];
-    return R.has(PROP_METADATA, constructor) && constructor[PROP_METADATA];
-};
-
-const getPropMeta = (comp: any, propName: string) => {
-    const meta = getMeta(comp);
-    if (!meta) {
-        return undefined;
-    }
-    return R.has(propName, meta) && meta[propName];
-};
-
-const isPropInput = (comp: any, propName: string) => {
-    const meta = getPropMeta(comp, propName);
-    return meta && R.find(R.propEq(NG_METADATA_NAME, NG_METADATA_NAME_INPUT), meta);
-};
-
-const isPropOutput = (comp: any, propName: string) => {
-    const meta = getPropMeta(comp, propName);
-    return meta && R.find(R.propEq(NG_METADATA_NAME, NG_METADATA_NAME_OUTPUT), meta);
-};
-
-const setComponentProperty = (cdr: ChangeDetectorRef, destroy$: Observable<any>, comp: any) => (
-    val: any,
-    key: string
-) => {
-    if (isPropOutput(comp, key)) {
-        if (!(comp[key] instanceof EventEmitter)) {
-            throw new Error('Output property must have EventEmitter type');
-        }
-        if (!(val instanceof Subject)) {
-            throw new Error('For Output properties, field property must have Subject type');
-        }
-        // dispatch from output to subject
-        (comp[key] as EventEmitter<any>)
-            .asObservable()
-            .pipe(takeUntil(destroy$))
-            .subscribe(x => {
-                (val as Subject<any>).next(x);
-            });
-        return;
-    }
-
-    if (isPropInput(comp, key)) {
-        if (val instanceof Observable) {
-            val.pipe(takeUntil(destroy$)).subscribe(x => {
-                comp[key] = x;
-                cdr.markForCheck();
-            });
-            return;
-        }
-
-        comp[key] = val;
-        return;
-    }
-};
-
-const setComponentProperties = (
-    cdr: ChangeDetectorRef,
-    destroy$: Observable<any>,
-    component: any,
-    field: IFormGroup<any>
-) => {
-    R.pipe(
-        R.omit(['$content', 'kind']),
-        R.forEachObjIndexed(setComponentProperty(cdr, destroy$, component))
-    )(field);
-};
-*/
+import { setComponentProperties } from '../set-component-properties';
 
 export interface GroupsLayoutMap {
     [key: string]: Type<any>;
@@ -104,16 +28,14 @@ export const HLC_GROUPS_LAYOUT = new InjectionToken<GroupsLayoutMap>('HLC_GROUPS
     selector: '[hlcGroupLayoutHost]'
 })
 export class GroupLayoutHostDirective implements OnInit, OnDestroy {
+    private componentRefs: ComponentRef<any>[];
     private destroy$ = new Subject();
     private groupsLayoutMap: GroupsLayoutMap;
 
     // tslint:disable-next-line:no-input-rename
     @Input('hlcGroupLayoutHost') group: IFormGroup<any>;
-
     // tslint:disable-next-line:no-input-rename
     @Input('hlcGroupLayoutHostForm') form: FormGroup;
-
-    // tslint:disable-next-line:no-input-rename
 
     constructor(
         private readonly componentFactoryResolver: ComponentFactoryResolver,
@@ -131,25 +53,31 @@ export class GroupLayoutHostDirective implements OnInit, OnDestroy {
         if (!this.group) {
             return;
         }
-        this.init(this.vcr, this.group);
-        // setTimeout(() => this.init(), 0);
+        this.componentRefs = this.init(this.vcr, this.group);
     }
 
-    init(container: ViewContainerRef, group: IFormGroup<any>) {
+    init(container: ViewContainerRef, group: IFormGroup<any>): ComponentRef<any>[] {
         const factory = this.componentFactoryResolver.resolveComponentFactory(this.groupsLayoutMap[group.kind]);
         const componentRef = factory.create(this.injector);
         const view = componentRef.hostView;
         container.insert(view);
 
-        // setComponentProperties(this.cdr, this.destroy$, this.componentRef.instance, this.group);
+        setComponentProperties(
+            ['id', 'kind'],
+            factory,
+            componentRef.changeDetectorRef,
+            this.destroy$,
+            componentRef.instance,
+            this.group
+        );
 
-        for (const child of group.$content) {
-            // console.log(this.componentRef);
-            this.init(componentRef.instance['vc'], child);
-        }
+        const crfs = R.chain(child => this.init(componentRef.instance['vc'], child), group.$content);
+
+        return [...crfs, componentRef];
     }
 
     ngOnDestroy() {
         this.destroy$.next();
+        this.componentRefs.forEach(crf => crf.destroy());
     }
 }
