45d5c8fba785db71c35c666856a5cfa8559d5472
baio <max.putilov@gmail.com>
2018-10-04 22:00:19 +0300

    fix build

--- libs/clr-forms/src/lib/index.ts
+++ libs/clr-forms/src/lib/index.ts
@@ -1,5 +1,4 @@
 export * from './clr-forms.module';
-export * from './form-footer';
-export * from './form-layout/form-layout.component';
 export * from './fields-layout';
+export * from './form-footer';
 export * from './input-container';
