adecc1e29c502c86113e7f16d600d636af5ab425
baio <max.putilov@gmail.com>
2018-11-21 09:45:00 +0300

    hide optional when input readonly

--- libs/clr-forms/src/lib/input-container/input-container.component.ts
+++ libs/clr-forms/src/lib/input-container/input-container.component.ts
@@ -19,6 +19,8 @@ export class InputContainerComponent implements OnInit, OnDestroy {
     id: string;
     @Input()
     validatorsErrorsMap: ClrFormFields.FieldValidatorsErrorsMap | undefined;
+    @Input()
+    readonly: boolean;
 
     @Input()
     formControl: FormControl;
@@ -53,7 +55,7 @@ export class InputContainerComponent implements OnInit, OnDestroy {
     }
 
     get isOptional() {
-        if (!this.control) {
+        if (!this.control || this.readonly) {
             return false;
         }
 
