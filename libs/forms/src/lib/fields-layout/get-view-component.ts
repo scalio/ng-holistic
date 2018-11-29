609618debddb5aa643c3ef899d14eb8dcf9c7c77
baio <max.putilov@gmail.com>
2018-11-29 18:52:11 +0300

    fix forms build

--- libs/forms/src/lib/fields-layout/get-view-component.ts
+++ libs/forms/src/lib/fields-layout/get-view-component.ts
@@ -1,5 +1,5 @@
 import { ViewRef } from '@angular/core';
-import { FormFields } from '@ng-holistic/forms';
+import { FormFields } from '../models';
 
 /**
  * Hackish way to retrieve components from View created by template projection.
