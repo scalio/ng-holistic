80a8a619d5747f15d42811ae790aabf680475a92
baio <max.putilov@gmail.com>
2018-11-30 10:38:37 +0300

    bump versions

--- apps/sandbox/src/app/app.module.ts
+++ apps/sandbox/src/app/app.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ClrLayoutModule, ClrMainContainerModule } from '@clr/angular';
 import { ClrFormModule, InputContainerModule, ValidationErrorsMapConfig } from '@ng-holistic/clr-forms';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { NxModule } from '@nrwl/nx';
-import { DndModule } from 'ng2-dnd';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routes } from './app.routing.module';
 
@@ -22,12 +17,7 @@ const inputContainerValdationErrorsMapConfig: ValidationErrorsMapConfig = {
         AppRoutingModule,
         ClrMainContainerModule,
         ClrLayoutModule,
-        NxModule.forRoot(),
         RouterModule.forRoot(routes, { initialNavigation: 'enabled', useHash: true }),
-        StoreModule.forRoot({}),
-        EffectsModule.forRoot([]),
-        StoreDevtoolsModule.instrument(),
-        DndModule.forRoot(),
         ClrFormModule.forRoot(),
         InputContainerModule.forRoot({ validationErrorsMap: inputContainerValdationErrorsMapConfig })
     ],
