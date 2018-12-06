40f7db673054890eb06d5f0535c62bfc1c179008
baio <max.putilov@gmail.com>
2018-12-06 23:24:58 +0300

    fix build

--- libs/clr-controls/src/lib/mask/mask.module.ts
+++ libs/clr-controls/src/lib/mask/mask.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { TextMaskModule } from 'angular2-text-mask';
+// import { TextMaskModule } from 'angular2-text-mask';
 import { MaskComponent } from './mask.component';
 import { FormsModule } from '@angular/forms';
+import { MaskedInputDirective } from './angular2TextMask';
 
 @NgModule({
-    imports: [CommonModule, TextMaskModule, FormsModule],
-    declarations: [MaskComponent],
-    exports: [MaskComponent]
+    imports: [CommonModule, FormsModule],
+    declarations: [MaskComponent, MaskedInputDirective],
+    exports: [MaskComponent, MaskedInputDirective]
 })
 export class MaskModule {}
--- libs/clr-controls/src/lib/mask/mask.utils.ts
+++ libs/clr-controls/src/lib/mask/mask.utils.ts
@@ -1,18 +1,41 @@
 import * as R from 'ramda';
 
 export class TextMask {
-    static unmaskStrNumber = (val: string): string | null => (val ? val.replace(/\D|\.+/g, '') : null);
-    static unmaskNumber = (val: string): number | null => {
+    static unmaskStrNumber(val: string): string | null {
+        const regex = /\D|\.+/g;
+        return val ? val.replace(regex, '') : null;
+    }
+
+    static unmaskNumber(val: string): number | null {
         const str = TextMask.unmaskStrNumber(val);
         return str ? +str : null;
-    };
+    }
 
-    static int = (n: number) => R.repeat(/\d/, n);
-    static float = (n: number) => R.repeat(/\d|\./, n);
-    static hexColor = () => ['#', ...R.repeat(/\d|a|b|c|d|e|f|A|B|C|D|E|F/, 6)];
-    static phone = () => ({
-        mask: ['+', /\d/, ' ', '(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
-        keepCharPositions: true,
-        placeholderChar: '\u2000'
-    });
+    static int(n: number) {
+        const regex = /\d/;
+        return R.repeat(regex, n);
+    }
+
+    static float(n: number) {
+        const regex = /\d|\./;
+        R.repeat(regex, n);
+    }
+    static hexColor() {
+        const regex = /\d|a|b|c|d|e|f|A|B|C|D|E|F/;
+        return ['#', ...R.repeat(regex, 6)];
+    }
+
+
+    static phone() {
+        // PIZDEC NAHOY BLYAT
+        // https://github.com/angular/angular/issues/18867
+        const d = /\d/;
+        const d19 = /[1-9]/;
+        return {
+            mask: ['+', d, ' ', '(', d19, d, d, ')', ' ', d, d, d, '-', d, d, d, d],
+            keepCharPositions: true,
+            placeholderChar: '\u2000'
+        };
+    }
+
 }
