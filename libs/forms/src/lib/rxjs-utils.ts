887e8b1977553f8f1f8ad162ebd355ac331aea51
baio <max.putilov@gmail.com>
2018-12-17 23:27:30 +0300

    propsChanged + delayFirst

--- libs/forms/src/lib/rxjs-utils.ts
+++ libs/forms/src/lib/rxjs-utils.ts
@@ -1,6 +1,21 @@
-import { Observable } from 'rxjs';
-import { eqProps, prop } from 'ramda';
-import { map, distinctUntilChanged } from 'rxjs/operators';
+import { eqProps, prop, equals } from 'ramda';
+import { combineLatest, Observable, merge } from 'rxjs';
+import { distinctUntilChanged, map, take, delay, skip } from 'rxjs/operators';
+
+/**
+ * When field is recallculated and this is fisrt calculation after form init,
+ * there could be value changed after check exception
+ * since [init value changed] -> [layout fisrt detectChanges] -> recalc -> new value on component.
+ * Delay first recalc with this method.
+ */
+export const delayFirst = <T>($: Observable<T>): Observable<T> => {
+    const head$ = $.pipe(
+        take(1),
+        delay(0)
+    );
+    const tail$ = $.pipe(skip(1));
+    return merge(head$, tail$);
+};
 
 /**
     Emits object only if property on object was changed
@@ -8,6 +23,7 @@ import { map, distinctUntilChanged } from 'rxjs/operators';
 */
 export const distinctPropChanged = <P extends keyof T, T>(propName: P) => ($: Observable<T>): Observable<T> =>
     $.pipe(
+        delayFirst,
         distinctUntilChanged(eqProps(propName as string) as any)
     ) as any;
 
@@ -17,6 +33,14 @@ export const distinctPropChanged = <P extends keyof T, T>(propName: P) => ($: Ob
  */
 export const propChanged = <P extends keyof T, T>(propName: P) => ($: Observable<T>): Observable<T[P]> =>
     $.pipe(
+        delayFirst,
         distinctPropChanged(propName),
         map(prop(propName as any) as any)
     ) as any;
+
+/**
+ * Emits property values only if properties on object was changed
+ * @param propNames - name of the property
+ */
+export const propsChanged = (...propNames: string[]) => ($: Observable<any>): Observable<any[]> =>
+    combineLatest(...propNames.map(p => propChanged<any, any>(p)($))).pipe(distinctUntilChanged(equals)) as any;
