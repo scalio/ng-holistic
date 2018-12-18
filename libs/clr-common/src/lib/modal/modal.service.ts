8a89758e8f9ef99464b18353afb449a7b0cfa8ea
baio <max.putilov@gmail.com>
2018-12-18 23:54:26 +0300

    fix AlertType

--- libs/clr-common/src/lib/modal/modal.service.ts
+++ libs/clr-common/src/lib/modal/modal.service.ts
@@ -3,10 +3,11 @@ import { FormGroup } from '@angular/forms';
 import { Observable, of, Subject } from 'rxjs';
 import { flatMap, map, shareReplay, take, takeUntil } from 'rxjs/operators';
 import { FormFooterDataAccess } from '../form-footer/form-footer.component';
-import { AlertModalComponent, AlertType } from './alert-modal/alert-modal.component';
+import { AlertModalComponent } from './alert-modal/alert-modal.component';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { FormProvider, ModalComponent } from './modal/modal.component';
 import { OverlayService } from './overlay.service';
+import { AlertType } from '../common.types';
 
 export interface ModalShowParams {
     title: string;
