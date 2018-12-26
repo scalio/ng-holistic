c19163c717c7676faf97a1e012640407a1d1ffb3
baio <max.putilov@gmail.com>
2018-12-26 12:47:22 +0300

    тгьиук зшзу

--- libs/clr-common/src/lib/pipes/common-pipes.module.ts
+++ libs/clr-common/src/lib/pipes/common-pipes.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { NumberPipe } from './number.pipe';
 import { PhonePipe } from './phone.pipe';
 
 @NgModule({
     imports: [],
-    declarations: [PhonePipe],
-    exports: []
+    declarations: [PhonePipe, NumberPipe],
+    exports: [PhonePipe, NumberPipe]
 })
 export class HlcPipesModule {
     constructor() {}
