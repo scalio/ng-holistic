2c375920f56e8ab4822d46792ec522c1642165a3
baio <max.putilov@gmail.com>
2019-01-25 22:49:13 +0300

    Rename project: sandbox -> clr-forms-sandbox #11

--- apps/clr-forms-sandbox/karma.conf.js
+++ apps/clr-forms-sandbox/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
     ...baseConfig,
     coverageIstanbulReporter: {
       ...baseConfig.coverageIstanbulReporter,
-      dir: join(__dirname, '../../coverage/apps/sandbox')
+      dir: join(__dirname, '../../coverage/apps/clr-forms-sandbox')
     }
   });
 };
