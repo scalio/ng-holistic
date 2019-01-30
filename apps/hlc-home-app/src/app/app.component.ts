f6d58e3e3fe5541a80f5089ba4f312c7ff491169
baio <max.putilov@gmail.com>
2019-01-30 22:57:16 +0300

    hlc-home-app #16

---
+++ apps/hlc-home-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ng-holistic-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    title = 'hlc-home-app';
+}
