4fd68e008b376d7de9e31368a6a40d45ed75e76b
baio <max.putilov@gmail.com>
2019-02-22 00:28:36 +0300

    forms local prod build

--- apps/clr-forms-sandbox/src/environments/environment.prod.ts
+++ apps/clr-forms-sandbox/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-    production: true
+    production: true,
+    appUrls: {
+        home: 'http://ng-holistic.com',
+        lists: 'http://lists.ng-holistic.com',
+        forms: 'http://forms.ng-holistic.com',
+        common: 'http://common.ng-holistic.com'
+    }
 };
