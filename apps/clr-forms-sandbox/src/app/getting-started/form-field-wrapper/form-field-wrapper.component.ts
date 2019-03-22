b265e72315bf8a2ad6402810e1f49e6d42539178
baio <max.putilov@gmail.com>
2019-03-22 17:10:22 +0300

    form-field-wrapper doc

--- apps/clr-forms-sandbox/src/app/getting-started/form-field-wrapper/form-field-wrapper.component.ts
+++ apps/clr-forms-sandbox/src/app/getting-started/form-field-wrapper/form-field-wrapper.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+const inputContainerConfig1 = `
+    export const inputContainerConfig: InputContainerConfig = {
+        return {
+            showRequiredHint: 'optional',
+            optionalLabel: 'optional'
+        };
+    }
+
+    @NgModule({
+        declarations: [AppComponent],
+        imports: [
+        ],
+        providers: [{
+
+        }]
+    })
+    export class AppModule {
+        constructor() {}
+    }
+
+`;
 
 @Component({
-  selector: 'hlc-form-field-wrapper',
-  templateUrl: './form-field-wrapper.component.html',
-  styleUrls: ['./form-field-wrapper.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'hlc-form-field-wrapper',
+    templateUrl: './form-field-wrapper.component.html',
+    styleUrls: ['./form-field-wrapper.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FormFieldWrapperComponent implements OnInit {
+    inputContainerConfig1 = inputContainerConfig1;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
