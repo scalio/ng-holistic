c6986de3c79e4707d981e1b64e643b0947aa3b11
baio <max.putilov@gmail.com>
2019-04-12 12:13:45 +0300

    fix home app build

--- apps/hlc-home-app/src/app/app.module.ts
+++ apps/hlc-home-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HlcHotKeysModule } from '@ng-holistic/clr-common';
 import { AppLayoutModule } from './app-layout/app-layout.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routes } from './app.routing.module';
@@ -11,7 +12,8 @@ import { AppRoutingModule, routes } from './app.routing.module';
         BrowserModule,
         AppLayoutModule,
         AppRoutingModule,
-        RouterModule.forRoot(routes, { initialNavigation: 'enabled', useHash: true })
+        RouterModule.forRoot(routes, { initialNavigation: 'enabled', useHash: true }),
+        HlcHotKeysModule.forRoot()
     ],
     providers: [],
     bootstrap: [AppComponent]
