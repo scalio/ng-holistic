5e280e36b1d1284ed9cc25f123b5e03ac4c08a6a
baio <max.putilov@gmail.com>
2019-05-21 14:21:52 +0300

    load-list-decorator - resetPageIndex

--- libs/clr-list/src/lib/repository/load-list-decorator.ts
+++ libs/clr-list/src/lib/repository/load-list-decorator.ts
@@ -21,6 +21,12 @@ export interface ILoadListDecorator<TState, TResult> {
 export type CheckInitRequestFun = (request: any) => boolean;
 
 /**
+ * Given stored request map it to one without page index.
+ * Initial request must always load first page data.
+ */
+export type ResetPageIndexFun = (request: any) => any;
+
+/**
  * When state is null or empty object, get latest state from storage and use one for request.
  * Optionally could use checkInitState - function which check if state is initial in which case
  * override initial state with stored one if it exists
@@ -29,6 +35,7 @@ export type CheckInitRequestFun = (request: any) => boolean;
 export class GetLoadListDecorator<TState, TResult> implements ILoadListDecorator<TState, TResult> {
     constructor(
         private readonly storage: IRepositoryStorage,
+        private readonly resetPageIndex: ResetPageIndexFun,
         private readonly checkInitRequest?: CheckInitRequestFun
     ) {}
     decorate(fn: LoadListFunc<TState, TResult>): LoadListFunc<TState, TResult> {
@@ -44,6 +51,8 @@ export class GetLoadListDecorator<TState, TResult> implements ILoadListDecorator
                         request
                     );
                     initialRequest = storedRequest.request;
+                    // lets reset pageIndex, initial request always should starts with 1st page
+                    initialRequest = this.resetPageIndex(initialRequest);
                 }
             }
 
@@ -60,13 +69,19 @@ export class GetLoadListDecorator<TState, TResult> implements ILoadListDecorator
 }
 
 export class GetAllSessionStorageDecorator<TState = any, TResult = any> extends GetLoadListDecorator<TState, TResult> {
-    constructor(name: string, map?: MapResultToRequestFun, ttl?: number) {
-        super(new RepositorySessionStorage(name, map, ttl));
+    constructor(name: string, resetPageIndex: ResetPageIndexFun, map?: MapResultToRequestFun, ttl?: number) {
+        super(new RepositorySessionStorage(name, map, ttl), resetPageIndex);
     }
 }
 
 export class GetAllLocalStorageDecorator<TState = any, TResult = any> extends GetLoadListDecorator<TState, TResult> {
-    constructor(name: string, map?: MapResultToRequestFun, checkInitState?: CheckInitRequestFun, ttl?: number) {
-        super(new RepositoryLocalStorage(name, map, ttl), checkInitState);
+    constructor(
+        name: string,
+        resetPageIndex: ResetPageIndexFun,
+        map?: MapResultToRequestFun,
+        checkInitState?: CheckInitRequestFun,
+        ttl?: number
+    ) {
+        super(new RepositoryLocalStorage(name, map, ttl), resetPageIndex, checkInitState);
     }
 }
