748758cd11cb8153ae5a2ca71ff30d02f5871ac0
baio <max.putilov@gmail.com>
2019-05-24 15:19:31 +0300

    table page

--- apps/clr-list-sandbox/src/app/clr-table/table-page/table-page.module.ts
+++ apps/clr-list-sandbox/src/app/clr-table/table-page/table-page.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { TablePageComponent } from './table-page.component';
-import { HlcClrTableModule } from '@ng-holistic/clr-list';
+import { AppsSharedModule } from '@apps/shared';
 import { HlcBoxModule } from '@ng-holistic/clr-common';
+import { HlcClrTableModule } from '@ng-holistic/clr-list';
+import { TablePageComponent } from './table-page.component';
 
 @NgModule({
     declarations: [TablePageComponent],
-    imports: [CommonModule, HlcClrTableModule, HlcBoxModule],
+    imports: [CommonModule, HlcClrTableModule, HlcBoxModule, AppsSharedModule],
     exports: [TablePageComponent]
 })
 export class TablePageModule {}
