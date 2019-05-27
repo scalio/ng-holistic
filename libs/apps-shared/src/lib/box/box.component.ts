b35f1adba8c8f6ba2d88f0d15dac16b1a6d1b76a
baio <max.putilov@gmail.com>
2019-05-27 15:00:23 +0300

    box-content

--- libs/apps-shared/src/lib/box/box.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'hlc-box',
-    templateUrl: './box.component.html',
-    styleUrls: ['./box.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class FmBoxComponent {}
