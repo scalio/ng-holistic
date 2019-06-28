bdf15d4df7f304b58f271b1c6e6809c678ecf3c3
baio <max.putilov@gmail.com>
2019-06-28 15:53:18 +0300

    docs(hlc-list): table row-action example

--- apps/clr-list-sandbox/src/app/clr-table/table-row-actions-page/table-row-actions-page.component.ts
+++ apps/clr-list-sandbox/src/app/clr-table/table-row-actions-page/table-row-actions-page.component.ts
@@ -68,6 +68,68 @@ const dataProvider: Table.Data.DataProvider = {
     }
 };
 
+
+const definition = `
+import { Table, TableDescription } from '@ng-holistic/clr-list';
+import { timer, Subject } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+import * as R from 'ramda';
+
+const table: TableDescription = {
+    cols: [
+        {
+            id: 'title',
+            title: 'Title',
+            sort: true
+        },
+        {
+            id: 'amount',
+            title: 'Amount',
+            sort: false
+        },
+        {
+            id: 'capTitle',
+            title: 'Title*',
+            format(_, row) {
+                return row['title'].toUpperCase();
+            }
+        },
+        {
+            id: 'link',
+            kind: 'LinkColumn',
+            title: 'Link',
+            props: {
+                title(_, row) {
+                    return row['title'];
+                },
+                link: 'some link',
+                clicked: new Subject()
+            }
+        },
+        {
+            id: 'custom',
+            customCell: true,
+            title: 'Custom'
+        }
+    ],
+    rowActions: (row: TableRow) =>
+        row.id === '2' ? [{ id: 'remove', title: 'Remove' }, { id: 'edit', title: 'Edit' }] : []
+};`;
+
+const code = `
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Table, TableDescription } from '@ng-holistic/clr-list';
+import { timer, Subject } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+import * as R from 'ramda';
+import { table } from './table-definition';
+
+const dataProvider: Table.Data.DataProvider = {
+    load(_) {
+        return timer(0).pipe(mapTo({ rows }));
+    }
+};
+
 @Component({
     selector: 'hlc-clr-sandbox-table-row-actions-page',
     templateUrl: './table-row-actions-page.component.html',
@@ -75,6 +137,30 @@ const dataProvider: Table.Data.DataProvider = {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableRowActionsPageComponent implements OnInit {
+    table = table;
+    dataProvider = dataProvider;
+
+    constructor() {}
+
+    ngOnInit() {}
+
+    onRowAction(rowAction: Table.RowAction) {
+        console.log('onRowAction', rowAction);
+    }
+}
+`;
+
+@Component({
+    selector: 'hlc-clr-sandbox-table-row-actions-page',
+    templateUrl: './table-row-actions-page.component.html',
+    styleUrls: ['./table-row-actions-page.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TableRowActionsPageComponent implements OnInit {
+
+    definition = definition;
+    code = code;
+
     table = table;
     dataProvider = dataProvider;
 
