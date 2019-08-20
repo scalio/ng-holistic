a4fe4ad299b9675c482e6d018e65d81f98ce3206
baio <max.putilov@gmail.com>
2019-08-20 17:56:59 +0300

    docs(modal): modal docs

--- apps/clr-common-sandbox/src/app/clr-modal/modal-page/modal-page.module.ts
+++ apps/clr-common-sandbox/src/app/clr-modal/modal-page/modal-page.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AppsSharedModule } from '@apps/shared';
 import { ClrInputModule } from '@clr/angular';
 import { HlcClrModalModule } from '@ng-holistic/clr-common';
 import { HlcClrFormModule } from '@ng-holistic/clr-forms';
@@ -9,7 +10,15 @@ import { ModalPageComponent, ModalPageFormComponent } from './modal-page.compone
 
 @NgModule({
     declarations: [ModalPageComponent, ModalPageFormComponent],
-    imports: [CommonModule, HlcClrModalModule, ReactiveFormsModule, HlcClrFormModule, ClrInputModule, RouterModule],
+    imports: [
+        CommonModule,
+        HlcClrModalModule,
+        ReactiveFormsModule,
+        HlcClrFormModule,
+        ClrInputModule,
+        RouterModule,
+        AppsSharedModule
+    ],
     exports: [ModalPageComponent],
     entryComponents: [ModalPageFormComponent]
 })
