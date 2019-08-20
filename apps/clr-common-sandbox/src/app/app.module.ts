1ff900e4125a65017e10e538b0ac1ad4c04d8cab
baio <max.putilov@gmail.com>
2019-08-20 13:23:59 +0300

    docs(common): restore common app

--- apps/clr-common-sandbox/src/app/app.module.ts
+++ apps/clr-common-sandbox/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HotkeyModule } from 'angular2-hotkeys';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' }), AppRoutingModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+        AppRoutingModule,
+        HotkeyModule.forRoot()
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
