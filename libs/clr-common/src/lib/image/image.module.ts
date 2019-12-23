912059dd4ea5ed2fb540be4a42c05e8680fe1fed
baio <max.putilov@gmail.com>
2019-12-23 19:21:28 +0300

    feat(image-upload): crop images

--- libs/clr-common/src/lib/image/image.module.ts
+++ libs/clr-common/src/lib/image/image.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { ClrIconModule } from '@clr/angular';
 import { HlcFilePreviewOverlayModule } from '../file-preview-overlay/file-preview-overlay.module';
 import { HlcClrImgOverlayModule } from '../img-overlay/img-overlay.module';
+import { ImageCropperModule } from '../ngx-image-cropper/image-cropper.module';
 import { HlcClrImageComponent } from './image.component';
 
 @NgModule({
     declarations: [HlcClrImageComponent],
     exports: [HlcClrImageComponent],
-    imports: [CommonModule, ClrIconModule, HlcClrImgOverlayModule, HlcFilePreviewOverlayModule],
+    imports: [CommonModule, ClrIconModule, HlcClrImgOverlayModule, HlcFilePreviewOverlayModule, ImageCropperModule],
     providers: []
 })
 export class HlcClrImageModule {}
---
+++ libs/clr-common/src/lib/ngx-image-cropper/interfaces/image-cropped-event.interface.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+import { CropperPosition } from "./cropper-position.interface";
+import { Transformations } from './transformations.interfaces';
+
+export interface ImageCroppedEvent {
+    base64?: string | null;
+    file?: Blob | null;
+    width: number;
+    height: number;
+    cropperPosition: CropperPosition;
+    imagePosition: CropperPosition;
+    offsetImagePosition?: CropperPosition;
+    transform: Transformations;
+}
